061f096bd2eac4731a77417ef6aa248d0080dc4e
Kessir Adjaho <kessir.adjaho@cloudator.com>
2019-01-12 16:48:11 +0200

    deploy

---
+++ precache-manifest.cb617e9463baf9cd689bc0f8d7148fbc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8961889b739015ad31b5",
+    "url": "/taxcalculatorgh/css/app.fe2b29ec.css"
+  },
+  {
+    "revision": "8961889b739015ad31b5",
+    "url": "/taxcalculatorgh/js/app.8961889b.js"
+  },
+  {
+    "revision": "c312b872f76716fdeb4f",
+    "url": "/taxcalculatorgh/js/chunk-vendors.c312b872.js"
+  },
+  {
+    "revision": "0c3eea3fad95363173f3e738151ade96",
+    "url": "/taxcalculatorgh/index.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/taxcalculatorgh/robots.txt"
+  },
+  {
+    "revision": "5fe14a1f3eed6efcb895eb0aa0de2557",
+    "url": "/taxcalculatorgh/css/spectre.css"
+  }
+];
